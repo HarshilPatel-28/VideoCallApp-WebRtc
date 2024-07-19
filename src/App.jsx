2957ca6d8cac9883d2001eb484216050e6f5db83
Harshil Patel <hp950036@gmail.com>
2024-07-19 15:43:41 +0530

    Uploading Final Project

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { Routes,Route } from "react-router-dom";
+import Lobby from "./screens/Lobby";
+import Room from "./screens/Room";
+
+
+
+
+function App() {
+  
+
+  return (
+    <>
+    <Routes>
+      <Route path="/" element={<Lobby/>}/>
+      <Route path="/room/:roomId" element={<Room/>}/>
+    </Routes>
+      
+    </>
+  )
+}
+
+export default App
